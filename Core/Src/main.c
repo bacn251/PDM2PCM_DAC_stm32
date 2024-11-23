c1d54f12c972e109d3a2d3e54b30466c26887afa
bacn251 <bacn251@gmail.com>
2024-11-23 16:28:54 +0700

    update fft

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -24,11 +24,20 @@
 /* USER CODE BEGIN Includes */
 #include "cs43l22.h"
 #include "AUDIO_LINK.h"
+#include <stdio.h>
+#include <string.h>
+#include "arm_math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+ set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -37,7 +46,18 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
+#define WAV_WRITE_SAMPLE_COUNT 256
+int16_t data_i2s[WAV_WRITE_SAMPLE_COUNT];
+float32_t mic1_data1[WAV_WRITE_SAMPLE_COUNT / 4],
+    mic1_data2[WAV_WRITE_SAMPLE_COUNT / 4];
+float32_t data_out_fft1[WAV_WRITE_SAMPLE_COUNT / 4],
+    data_out_fft2[WAV_WRITE_SAMPLE_COUNT / 4];
+volatile int16_t sample_i2s;
+volatile uint8_t button_flag, start_stop_recording;
+volatile uint8_t half_i2s, full_i2s;
+float output_buffer[WAV_WRITE_SAMPLE_COUNT / 4];
+float magnitude_buffer[WAV_WRITE_SAMPLE_COUNT / 4];
+arm_rfft_fast_instance_f32 fft_instance;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -51,6 +71,8 @@ DMA_HandleTypeDef hdma_spi2_rx;
 DMA_HandleTypeDef hdma_spi3_tx;
 
 SPI_HandleTypeDef hspi1;
+
+UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
@@ -66,6 +88,7 @@ static void MX_I2S3_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_CRC_Init(void);
 static void MX_I2S2_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -78,18 +101,19 @@ int16_t MidBuffer[32];
 uint8_t txstate = 0;
 uint8_t rxstate = 0;
 
-
 int16_t fifobuf[256];
 uint8_t fifo_w_ptr = 0;
 uint8_t fifo_r_ptr = 0;
 uint8_t fifo_read_enabled = 0;
 
-void FifoWrite(int16_t data) {
+void FifoWrite(int16_t data)
+{
   fifobuf[fifo_w_ptr] = data;
   fifo_w_ptr++;
 }
 
-int16_t FifoRead() {
+int16_t FifoRead()
+{
   int16_t val = fifobuf[fifo_r_ptr];
   fifo_r_ptr++;
   return val;
@@ -97,13 +121,13 @@ int16_t FifoRead() {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+  uint8_t uart_counter;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -118,7 +142,7 @@ int main(void)
   /* Configure the system clock */
   SystemClock_Config();
 
-/* Configure the peripherals common clocks */
+  /* Configure the peripherals common clocks */
   PeriphCommonClock_Config();
 
   /* USER CODE BEGIN SysInit */
@@ -134,6 +158,7 @@ int main(void)
   MX_CRC_Init();
   MX_I2S2_Init();
   MX_PDM2PCM_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
   PDM_Filter_Handler_t PDM1_filter_handler;
@@ -141,21 +166,21 @@ int main(void)
   /* Initialize PDM Filter structure */
   PDM1_filter_handler.bit_order = PDM_FILTER_BIT_ORDER_LSB;
   PDM1_filter_handler.endianness = PDM_FILTER_ENDIANNESS_BE;
-  PDM1_filter_handler.high_pass_tap = 2136746228; //2104533974; //2136746228; //0.9xx*(2^31-1)
+  PDM1_filter_handler.high_pass_tap = 2136746228; // 2104533974; //2136746228; //0.9xx*(2^31-1)
   PDM1_filter_handler.out_ptr_channels = 1;
   PDM1_filter_handler.in_ptr_channels = 1;
-  PDM_Filter_Init ((PDM_Filter_Handler_t*) (&PDM1_filter_handler));
+  PDM_Filter_Init((PDM_Filter_Handler_t *)(&PDM1_filter_handler));
 
   PDM1_filter_config.output_samples_number = 32;
   PDM1_filter_config.mic_gain = 25;
-  PDM1_filter_config.decimation_factor = PDM_FILTER_DEC_FACTOR_32; //DAC CLK: 46875 kS/s * 32 bit = 1500000 MHz, PDM2PCM: 1500000 / 32 = 46875 kS/s
-  PDM_Filter_setConfig ((PDM_Filter_Handler_t*) &PDM1_filter_handler, &PDM1_filter_config);
+  PDM1_filter_config.decimation_factor = PDM_FILTER_DEC_FACTOR_32; // DAC CLK: 46875 kS/s * 32 bit = 1500000 MHz, PDM2PCM: 1500000 / 32 = 46875 kS/s
+  PDM_Filter_setConfig((PDM_Filter_Handler_t *)&PDM1_filter_handler, &PDM1_filter_config);
 
   uint16_t readid = 0, initret = 0;
   /* Retieve audio codec identifier */
   readid = cs43l22_drv.ReadID(AUDIO_I2C_ADDRESS); // & CS43L22_ID_MASK) == CS43L22_ID)
   initret = cs43l22_Init(AUDIO_I2C_ADDRESS, OUTPUT_DEVICE_BOTH, 80, AUDIO_FREQUENCY_48K);
-
+  arm_rfft_fast_init_f32(&fft_instance, WAV_WRITE_SAMPLE_COUNT / 4);
   /*for (int i = 0; i < 128; i=i+2)
   {
     txBuf[i] = 32767 * sin(2 * 3.14159265 * (float)i / 128.0);
@@ -164,7 +189,7 @@ int main(void)
 
   HAL_I2S_Transmit_DMA(&hi2s3, (uint16_t *)&txBuf[0], 128);
   HAL_I2S_Receive_DMA(&hi2s2, &pdmRxBuf[0], 128);
-
+  printf("uart start\n");
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -172,78 +197,104 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+	  if (button_flag)
+	  {
+	      if (start_stop_recording)
+	      {
+	          start_stop_recording = 0;
+	          printf("stop recording \n");
+	      }
+	      else
+	      {
+	          start_stop_recording = 1;
+	          printf("start recording \n");
+	      }
 
-    /* USER CODE BEGIN 3 */
-    if (rxstate == 1)
+	      button_flag = 0; // Xóa cờ sau khi xử lý
+	  }
+    if (rxstate == 1 &&start_stop_recording==1)
     {
-      PDM_Filter (&pdmRxBuf[0], &MidBuffer[0], &PDM1_filter_handler);
+      PDM_Filter(&pdmRxBuf[0], &MidBuffer[0], &PDM1_filter_handler);
+      for (int i = 0; i < 64; i++) // Cập nhật từ 32 thành 64
+      {
+        FifoWrite(MidBuffer[i]);
+        mic1_data1[i] = (float32_t)MidBuffer[i];
+      }
+      arm_rfft_fast_f32(&fft_instance, mic1_data1, output_buffer, 0);
+
+      // Tính biên độ từ FFT
       for (int i = 0; i < 32; i++)
       {
-        FifoWrite (MidBuffer[i]);
+    	float  magnitude = sqrtf(output_buffer[2 * i] * output_buffer[2 * i] +
+                                    output_buffer[2 * i + 1] * output_buffer[2 * i + 1]);
+        float frequency = i * (48000.0f / 64.0f); // f[i] = i * (fs / N)
+
+        printf("%.2f Hz: %.2f\r\n", frequency, magnitude);
       }
+
+      // Xuất kết quả ra UART
+//      printf("FFT Magnitude1: \r\n");
+//      for (int i = 0; i < (WAV_WRITE_SAMPLE_COUNT / 4); i++)
+//      {
+//        printf("%.2f \r\n", magnitude_buffer[i]);
+//      }
+//      printf("\n");
       if (fifo_w_ptr - fifo_r_ptr > 128)
         fifo_read_enabled = 1;
       rxstate = 0;
     }
 
-    if (rxstate == 2)
+    if (rxstate == 2&&start_stop_recording==1)
     {
-      PDM_Filter (&pdmRxBuf[64], &MidBuffer[0], &PDM1_filter_handler);
+      PDM_Filter(&pdmRxBuf[64], &MidBuffer[0], &PDM1_filter_handler);
+      for (int i = 0; i < 64; i++) // Cập nhật từ 32 thành 64
+      {
+        FifoWrite(MidBuffer[i]);
+        mic1_data1[i] = (float32_t)MidBuffer[i];
+      }
+      arm_rfft_fast_f32(&fft_instance, mic1_data1, output_buffer, 0);
+
+      // Tính biên độ từ FFT
       for (int i = 0; i < 32; i++)
       {
-        FifoWrite (MidBuffer[i]);
+    	float  magnitude = sqrtf(output_buffer[2 * i] * output_buffer[2 * i] +
+                                    output_buffer[2 * i + 1] * output_buffer[2 * i + 1]);
+        float frequency = i * (48000.0f / 64.0f); // f[i] = i * (fs / N)
+
+        printf("%.2f Hz: %.2f\r\n", frequency, magnitude); // Xuất tần số và biên độ
       }
+
+      // Xuất kết quả ra UART
+//      printf("FFT Magnitude2: ");
+//      for (int i = 0; i < (WAV_WRITE_SAMPLE_COUNT / 4); i++)
+//      {
+//        printf("%.2f \r\n", magnitude_buffer[i]);
+//      }
+//      printf("\n");
       rxstate = 0;
     }
-
-    if (txstate == 1)
-    {
-      if (fifo_read_enabled == 1)
-      {
-        for (int i = 0; i < 64; i = i + 2)
-        {
-          int16_t data = FifoRead ();
-          txBuf[i] = data;
-          txBuf[i + 1] = data;
-        }
-      }
-      txstate = 0;
-    }
-
-    if (txstate == 2)
-    {
-      if (fifo_read_enabled == 1)
-      {
-        for (int i = 64; i < 128; i = i + 2)
-        {
-          int16_t data = FifoRead ();
-          txBuf[i] = data;
-          txBuf[i + 1] = data;
-        }
-      }
-      txstate = 0;
-    }
+    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -258,9 +309,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
@@ -273,15 +323,15 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief Peripherals Common Clock Configuration
-  * @retval None
-  */
+ * @brief Peripherals Common Clock Configuration
+ * @retval None
+ */
 void PeriphCommonClock_Config(void)
 {
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
   /** Initializes the peripherals clock
-  */
+   */
   PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2S;
   PeriphClkInitStruct.PLLI2S.PLLI2SN = 192;
   PeriphClkInitStruct.PLLI2S.PLLI2SR = 2;
@@ -292,10 +342,10 @@ void PeriphCommonClock_Config(void)
 }
 
 /**
-  * @brief CRC Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief CRC Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_CRC_Init(void)
 {
 
@@ -315,14 +365,13 @@ static void MX_CRC_Init(void)
   /* USER CODE BEGIN CRC_Init 2 */
 
   /* USER CODE END CRC_Init 2 */
-
 }
 
 /**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_I2C1_Init(void)
 {
 
@@ -349,14 +398,13 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
-  * @brief I2S2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief I2S2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_I2S2_Init(void)
 {
 
@@ -383,14 +431,13 @@ static void MX_I2S2_Init(void)
   /* USER CODE BEGIN I2S2_Init 2 */
 
   /* USER CODE END I2S2_Init 2 */
-
 }
 
 /**
-  * @brief I2S3 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief I2S3 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_I2S3_Init(void)
 {
 
@@ -417,14 +464,13 @@ static void MX_I2S3_Init(void)
   /* USER CODE BEGIN I2S3_Init 2 */
 
   /* USER CODE END I2S3_Init 2 */
-
 }
 
 /**
-  * @brief SPI1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief SPI1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_SPI1_Init(void)
 {
 
@@ -455,12 +501,43 @@ static void MX_SPI1_Init(void)
   /* USER CODE BEGIN SPI1_Init 2 */
 
   /* USER CODE END SPI1_Init 2 */
-
 }
 
 /**
-  * Enable DMA controller clock
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+}
+
+/**
+ * Enable DMA controller clock
+ */
 static void MX_DMA_Init(void)
 {
 
@@ -474,19 +551,18 @@ static void MX_DMA_Init(void)
   /* DMA1_Stream5_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
@@ -503,8 +579,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(OTG_FS_PowerSwitchOn_GPIO_Port, OTG_FS_PowerSwitchOn_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOD, LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin
-                          |Audio_RST_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOD, LD4_Pin | LD3_Pin | LD5_Pin | LD6_Pin | Audio_RST_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : CS_I2C_SPI_Pin */
   GPIO_InitStruct.Pin = CS_I2C_SPI_Pin;
@@ -522,7 +597,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
@@ -534,8 +609,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : LD4_Pin LD3_Pin LD5_Pin LD6_Pin
                            Audio_RST_Pin */
-  GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin|LD5_Pin|LD6_Pin
-                          |Audio_RST_Pin;
+  GPIO_InitStruct.Pin = LD4_Pin | LD3_Pin | LD5_Pin | LD6_Pin | Audio_RST_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -548,7 +622,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(VBUS_FS_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : OTG_FS_ID_Pin OTG_FS_DM_Pin OTG_FS_DP_Pin */
-  GPIO_InitStruct.Pin = OTG_FS_ID_Pin|OTG_FS_DM_Pin|OTG_FS_DP_Pin;
+  GPIO_InitStruct.Pin = OTG_FS_ID_Pin | OTG_FS_DM_Pin | OTG_FS_DP_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -566,37 +640,57 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(MEMS_INT2_GPIO_Port, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 2, 0); // Đặt mức ưu tiên cho ngắt
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);         // Bật ngắt EXTI0
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_I2S_TxHalfCpltCallback (I2S_HandleTypeDef *hi2s)
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+  return ch;
+}
+void HAL_I2S_TxHalfCpltCallback(I2S_HandleTypeDef *hi2s)
 {
   txstate = 1;
 }
 
-void HAL_I2S_TxCpltCallback (I2S_HandleTypeDef *hi2s)
+void HAL_I2S_TxCpltCallback(I2S_HandleTypeDef *hi2s)
 {
   txstate = 2;
 }
 
-void HAL_I2S_RxHalfCpltCallback (I2S_HandleTypeDef *hi2s)
+void HAL_I2S_RxHalfCpltCallback(I2S_HandleTypeDef *hi2s)
 {
   rxstate = 1;
 }
 
-void HAL_I2S_RxCpltCallback (I2S_HandleTypeDef *hi2s)
+void HAL_I2S_RxCpltCallback(I2S_HandleTypeDef *hi2s)
 {
   rxstate = 2;
+}
+void EXTI0_IRQHandler(void)
+{
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0); // Xử lý ngắt qua HAL
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  if (GPIO_Pin == GPIO_PIN_0) // Kiểm tra ngắt từ PA0
+  {
+    button_flag = 1; // Đặt cờ khi nút được nhấn
+  }
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -608,14 +702,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
